48a96f003ccfadf13e27cc7f73e755c1eea0094a
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2020-11-08 18:53:51 +0545

    Update home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,20 @@
-<template></template>
+<template>
+  <div>
+    <create-joke></create-joke>
+    <jokes-list-view></jokes-list-view>
+  </div>
+</template>
 
 <script>
-export default {};
+// Import create component and list component
+import CreateJoke from "../components/CreateJoke.vue";
+import JokesListView from "../components/JokesListView.vue";
+export default {
+  components: {
+    CreateJoke,
+    JokesListView,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
