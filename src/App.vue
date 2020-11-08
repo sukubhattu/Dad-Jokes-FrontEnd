3ee3d74b25daa55e8ca5ef22eb76f4a65ddb061e
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2020-11-08 13:07:54 +0545

    Default vue cli outputs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <joke-header></joke-header>
+    <v-container class="mt-16">
+      <create-joke></create-joke>
+      <jokes-list-view></jokes-list-view>
+    </v-container>
+  </v-app>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./components/Header.vue";
+import CreateJoke from "./components/CreateJoke.vue";
+import JokesListView from "./components/JokesListView.vue";
+// import DetailView from "./components/DetailView.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    JokeHeader: Header,
+    CreateJoke: CreateJoke,
+    JokesListView: JokesListView,
+    // JokeDetailView: DetailView,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
 </style>
