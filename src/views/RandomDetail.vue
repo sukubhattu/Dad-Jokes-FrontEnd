19550f5e93f2a4d2cb68d2fcfbb5dbd05bddd463
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2020-11-08 18:53:00 +0545

    Add random page

---
+++ src/views/RandomDetail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div><h1>I am random page</h1></div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  mounted() {
+    axios
+      .get("http://localhost:8000/api/v1/random")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        if (err.response.status == 404) {
+          console.log("404 bitches");
+        } else {
+          console.log(err);
+        }
+      });
+  },
+};
+</script>
+
+<style>
+</style>
