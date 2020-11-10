9f9f7b9fb5eaed800ad3c9604d3fe0c3306c201f
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2020-11-10 17:00:10 +0545

    Add store and axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import router from './router'
+import router from "./router";
 
 Vue.config.productionTip = false;
 
+// Import axios and create a base URL
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:8000/";
+
+// import store
+import { store } from "./store/store";
 new Vue({
-    vuetify,
-    router,
-    render: (h) => h(App)
+	store,
+	vuetify,
+	router,
+	render: (h) => h(App),
 }).$mount("#app");
